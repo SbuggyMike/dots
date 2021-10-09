8f37eae253301effbcd4b51418e6010639152127
sbuggyMike <achillesubuntu@gmail.com>
2021-10-09 11:13:18 +0100

    Updated emacs stuff

---
+++ emacs/.emacs.d/elpa/org-journal-20210812.1749/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from org-journal.el  -*- no-byte-compile: t -*-
+(define-package "org-journal" "20210812.1749" "a simple org-mode based journaling mode" '((emacs "25.1") (org "9.1")) :commit "01efa117248622728d5aa88ab9b8c70c68b6d3eb" :authors '(("Bastian Bechtold") ("Christian Schwarzgruber")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
--- emacs/keybindings.el
+++ emacs/keybindings.el
@@ -1,16 +1,17 @@
 ; this is a list of keybindings
 
 ; music
-(global-set-key (kbd "C-c SPC") 'emms-pause)
-(global-set-key (kbd "C-c n") 'emms-next)
-(global-set-key (kbd "C-c N") 'emms-cue-next)
-(global-set-key (kbd "C-c p") 'emms-previous)
-(global-set-key (kbd "C-c P") 'emms-cue-previous)
-(global-set-key (kbd "C-c d") 'emms-add-directory)
-(global-set-key (kbd "C-c f") 'emms-add-file)
-(global-set-key (kbd "C-c b") 'emms-bookmarks-add)
-(global-set-key (kbd "C-c B") 'emms-bookmarks-next)
-(global-set-key (kbd "C-c s") 'emms-stop)
+(global-set-key (kbd "s-m SPC") 'emms-pause)
+(global-set-key (kbd "s-m n") 'emms-next)
+(global-set-key (kbd "s-m N") 'emms-cue-next)
+(global-set-key (kbd "s-m p") 'emms-previous)
+(global-set-key (kbd "s-m P") 'emms-cue-previous)
+(global-set-key (kbd "s-m d") 'emms-add-directory)
+(global-set-key (kbd "s-m f") 'emms-add-file)
+(global-set-key (kbd "s-m b") 'emms-bookmarks-add)
+(global-set-key (kbd "s-m B") 'emms-bookmarks-next)
+(global-set-key (kbd "s-m s") 'emms-stop)
+(global-set-key (kbd "s-m u") 'emms-add-url)
 
 ; email
 (global-set-key (kbd "C-c g") 'gnus)
@@ -24,11 +25,16 @@
 ; eww
 (global-set-key (kbd "C-c w") 'eww)
 
+;; buffer operations
 ; rename-buffer
-(global-set-key (kbd "C-c rb") 'rename-buffer)
+(global-set-key (kbd "C-c cr") 'rename-buffer)
 
-; load-file
-(global-set-key (kbd "C-c lf") 'load-file)
+; count-words
+(global-set-key (kbd "C-c cw") 'count-words)
+
+; org-agenda
+(global-set-key (kbd "C-c oa") 'org-agenda)
+
 
 ; when a new window is open, don't display the same buffer
 (defun better-horizontal-split ()
@@ -46,4 +52,11 @@
   (previous-buffer))
 (global-set-key (kbd "C-x 2") 'better-vertical-split)
 
+; load-file
+(global-set-key (kbd "C-c lf") 'load-file)
 
+; org mode shortcuts
+; make footnoting easier
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (define-key org-mode-map (kbd "C-c a") 'org-footnote-new)))
