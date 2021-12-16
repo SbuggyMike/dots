a6a148360d7fade965d2d3d6f0ea2102c360a0bf
sbuggyMike <achillesubuntu@gmail.com>
2021-12-16 11:05:12 +0000

    Latest update

--- emacs/.emacs.d/org-clock-save.el
+++ emacs/.emacs.d/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - ithaca at <2021-10-15 Fri 17:24>
-(setq org-clock-stored-history '(("/home/ajax/Nextcloud/life_org/todo.org" . 1)))
+;; org-clock-save.el - ithaca at <2021-12-08 Wed 16:27>
+(setq org-clock-stored-history '(("/home/ajax/Nextcloud/life_org/todo.org" . 46198) ("/home/ajax/Nextcloud/life_org/todo.org" . 47500) ("/home/ajax/Nextcloud/life_org/todo.org" . 47602) ("/home/ajax/Nextcloud/life_org/phd_schedule.org" . 1757) ("/home/ajax/Nextcloud/life_org/phd_schedule.org" . 4172)))
