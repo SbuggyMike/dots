bb265ceb7a9dcb7f55e274dc6b9698eee6595887
sbuggyMike <s.j.hawes@btinternet.com>
2022-02-14 10:04:20 +0000

    Deleted .emacs folder; added master-init; modified i3 config

--- emacs/.emacs.d/org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-clock-save.el - ithaca at <2021-12-08 Wed 16:27>
-(setq org-clock-stored-history '(("/home/ajax/Nextcloud/life_org/todo.org" . 46198) ("/home/ajax/Nextcloud/life_org/todo.org" . 47500) ("/home/ajax/Nextcloud/life_org/todo.org" . 47602) ("/home/ajax/Nextcloud/life_org/phd_schedule.org" . 1757) ("/home/ajax/Nextcloud/life_org/phd_schedule.org" . 4172)))
