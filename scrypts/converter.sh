af97999265e30f1edeeca69ab165cddcaff7e885
Stephen Hawes <s.j.hawes@btinternet.com>
2024-01-05 17:30:40 +0000

    regular commit on Fri  5 Jan 17:21:44 GMT 2024

--- scrypts/converter.sh
+++ scrypts/converter.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-find . -name "*.flac" -exec ffmpeg -i {} -ab 320k -filter:a "volume=3.0" -map_metadata 0 -id3v2_version 3 {}.mp3 \;
+find . -name "*.flac" -exec ffmpeg -i {} -ab 320k -filter:a "volume=1.5" -map_metadata 0 -id3v2_version 3 {}.mp3 \;
