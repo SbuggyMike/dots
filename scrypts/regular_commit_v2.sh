ca2fa57593f657734c5db884b9acc03e76b49c9e
Stephen Hawes <s.j.hawes@btinternet.com>
2024-08-25 11:48:12 +0100

    regular commit on Sun 25 Aug 11:47:54 BST 2024

--- scrypts/regular_commit_v2.sh
+++ scrypts/regular_commit_v2.sh
@@ -7,7 +7,7 @@ cp ~/.vimrc ~/dots/
 DATE=$(date)
 
 # List of repositories
-REPOS=("~/phd_law" "~/misc_law" "~/life_org" "/storage/spare_home/bvs" "~/dots" "~/roam" "/storage/spare_home/hustle")
+REPOS=("/home/telemachus/phd_law" "/home/telemachus/misc_law" "/home/telemachus/life_org" "/storage/spare_home/bvs" "/home/telemachus/dots" "/home/telemachus/roam" "/storage/spare_home/hustle")
 
 # Loop over each repository
 for REPO in "${REPOS[@]}"
