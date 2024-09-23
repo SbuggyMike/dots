a0db7b3c9bb7672659b6ca5b0b3b5c2914b1165e
Stephen Hawes <s.j.hawes@btinternet.com>
2024-09-23 18:34:21 +0100

    regular commit on Mon 23 Sep 18:34:17 BST 2024

---
+++ scrypts/regular_commit_v3.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Copy files over to dots
+cp ~/.vimrc ~/dots/
+
+# Get the current date
+DATE=$(date)
+
+# List of repositories
+REPOS=("/home/telemachus/phd_law" "/home/telemachus/misc_law" "/home/telemachus/life_org" "/storage/spare_home/bvs" "/home/telemachus/dots" "/home/telemachus/roam" "/storage/spare_home/hustle")
+
+# Loop over each repository
+for REPO in "${REPOS[@]}"
+do
+    # Navigate to the repository
+    cd $REPO
+    echo "Changed to" $(pwd)
+
+    # Add any new files
+    git add .
+
+    # Commit changes with a commit message of "regular commit on DATE"
+    git commit -m "regular commit on $DATE"
+
+    # Push the commit
+    git push
+done
